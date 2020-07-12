5b92c32d29e48932c8f5130160064ed2efc64ec2
adarsh kumar <kumar.adarsh50@gmail.com>
2020-07-12 16:41:18 +0530

    Updates

---
+++ precache-manifest.e34d84e15997cb4440bbdf58e6324fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1260d998b2d98c7a260f96a97aa40742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "092ce5987decdebe8b69",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9b9dce3b7204f595e47c",
+    "url": "/static/css/main.a21b7797.chunk.css"
+  },
+  {
+    "revision": "092ce5987decdebe8b69",
+    "url": "/static/js/2.929506a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.929506a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9dce3b7204f595e47c",
+    "url": "/static/js/main.77ed6184.chunk.js"
+  },
+  {
+    "revision": "e91398ec253ad8957855",
+    "url": "/static/js/runtime-main.8d771be7.js"
+  }
+]);
